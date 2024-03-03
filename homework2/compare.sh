937da16e921d20f8107ca90cf974b8bb802b3e4e
Lucas Lu <lucas89901@gmail.com>
2024-03-04 02:43:55 +0800

    homework2: Finish subtask 3 比較目錄

--- homework2/compare.sh
+++ homework2/compare.sh
@@ -4,6 +4,8 @@ HIDDEN=
 SYMLINK=
 REGEX=
 RECURSIVE=
+PATH_A=
+PATH_B=
 
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
@@ -51,6 +53,10 @@ parse_optional_args() {
     esac
   done
   readonly HIDDEN SYMLINK REGEX RECURSIVE
+  shift $(( OPTIND - 1 ))
+  PATH_A="$1"
+  PATH_B="$2"
+  readonly PATH_A PATH_B
   return $(( OPTIND - 1 ))
 }
 
@@ -59,17 +65,18 @@ parse_optional_args() {
 # Globals:
 #   SYMLINK
 # Arugments:
-#   A file path to be checked.
+#   A path of the file to be checked.
 # Returns:
-#  0 if file exists, 1 otherwise.
+#   0 if file exists, 1 otherwise.
 check_exist() {
   if [[ -L "$1" ]]; then
     if [[ -z "${SYMLINK}" ]]; then
-      usage
+      return 1
     fi
   elif ! [[ -e  "$1" ]]; then
-    usage
+    return 1
   fi
+  return 0
 }
 
 # Verify arguments.
@@ -116,23 +123,25 @@ max() {
   fi
 }
 
-main() {
-  err "args:" "$@"
-  parse_optional_args "$@"
-  shift $?
-  err "HIDDEN=${HIDDEN}, SYMLINK=${SYMLINK}, REGEX=${REGEX}, RECURSIVE=${RECURSIVE}"
-  err "positional args:" "$@"
-  verify_args "$@"
-
+# Compare two files.
+# Arguments:
+#   Paths of two files to compare.
+# Outputs:
+#   Nothing if files are identical, "changed X%" if files differ, where X is
+#   calculated according to the problem spec.
+# Returns:
+#   0 if files are identical, 1 if files differ.
+compare_files() {
   local diff_output
   # -U 0 is necessary for line calculations below to work.
   if diff_output="$(diff -d -U 0 "$1" "$2")"; then
     # No differences.
-    exit
+    return 0
   fi
 
-  if grep binary <(echo -E "${diff_output}"); then
+  if grep Binary <(echo -E "${diff_output}") > /dev/null; then
     echo "changed 100%"
+    return 1
   fi
 
   diff_output_tail="$(tail -n +3 <(echo -E "${diff_output}"))"  # Remove header
@@ -148,6 +157,86 @@ main() {
   local -i mx
   mx="$(max "${delete_count}" "${insert_count}")"
   echo "changed $(( 100 * mx / $(( mx + keep_count )) ))%"
+  return 1
+}
+
+# Compare paths pointed by two symbolic links.
+# Arguments:
+#   Paths of two symbolic links to compare.
+# Outputs:
+#   Nothing if symbolic links are identical, "changed 100%" otherwise.
+# Returns:
+#   0 if symbolic links point to the same path, 1 otherwise.
+compare_symlinks() {
+  if ! diff "$(readlink "$1")" "$(readlink   "$2")"; then
+    echo "changed 100%"
+  fi
+  return 0
+}
+
+# Recursively compare two directories.
+# Arguments:
+#   Paths of two directories to compare.
+# Outputs:
+#   If both files exist, output individual file paths followed by output of
+#   `compare_files()`.
+compare_directories() {
+  # Unique elements in array.
+  # Reference: https://stackoverflow.com/a/13649357
+  local -a temp
+  for f in "$1"/*; do
+    temp+=("${f#"$1"/}")
+  done
+  for f in "$2"/*; do
+    temp+=("${f#"$2"/}")
+  done
+  for f in "${temp[@]}"; do echo "${f}"; done | uniq > test.txt
+  readarray -t files < \
+      <(for f in "${temp[@]}"; do echo "${f}"; done | sort | uniq)
+  err "${files[@]}"
+
+  for f in "${files[@]}"; do
+    if [[ -d "$1/${f}" ]] && [[ -d "$2/${f}" ]]; then
+      compare_directories "$1/${f}" "$2/${f}"
+    else
+      output_path="$1/${f}"
+      output_path="${output_path#"${PATH_A}/"}"
+      output_path="${output_path#"${PATH_B}/"}"
+      err "output_path=${output_path}"
+      if [[ -L "$1/${f}" ]] && [[ -z "${SYMLINK}" ]]; then
+        continue
+      elif ! check_exist "$1/${f}" \
+           && check_exist "$2/${f}"; then
+        echo "create ${output_path}"
+        continue
+      elif check_exist "$1/${f}" \
+           && ! check_exist "$2/${f}"; then
+        echo "delete ${output_path}"
+        continue
+      fi
+
+      local compare_output
+      compare_output="$(compare_files "$1/${f}" "$2/${f}")"
+      if (( $? == 1 )); then
+        echo "${output_path}: ${compare_output}"
+      fi
+    fi
+  done
+}
+
+main() {
+  err "args:" "$@"
+  parse_optional_args "$@"
+  shift $?
+  err "HIDDEN=${HIDDEN}, SYMLINK=${SYMLINK}, REGEX=${REGEX}, RECURSIVE=${RECURSIVE}"
+  err "positional args:" "$@"
+  verify_args "$@"
+
+  if [[ -n "${RECURSIVE}" ]]; then
+    compare_directories "$1" "$2"
+  else
+    compare_files "$1" "$2"
+  fi
 }
 
 main "$@"
